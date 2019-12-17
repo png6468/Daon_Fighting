8ac0e3e2a5f71b4368b6b96aba51da3b38e5bcaa
kang-b <lsj060912@naver.com>
2019-12-17 11:07:19 +0900

    쿼리 등록 및 서비스, 다오 세팅

--- Daon_Team_Project/src/main/java/com/min/edu/model/ByeongDaon_DaoImpl.java
+++ Daon_Team_Project/src/main/java/com/min/edu/model/ByeongDaon_DaoImpl.java
@@ -1,5 +1,38 @@
 package com.min.edu.model;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.min.edu.dtos.Admin_Dto;
+
+@Repository
 public class ByeongDaon_DaoImpl implements ByeongDaon_IDao {
 
+	@Override
+	public String idCheckAdmin(String adm_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin_Dto login_Admin(Map<String, String> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin_Dto myPageAdmin(String adm_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Admin_Dto> allAdmin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
--- Daon_Team_Project/src/main/java/com/min/edu/model/ByeongDaon_IService.java
+++ Daon_Team_Project/src/main/java/com/min/edu/model/ByeongDaon_IService.java
@@ -1,5 +1,14 @@
 package com.min.edu.model;
 
+import java.util.List;
+import java.util.Map;
+
+import com.min.edu.dtos.Admin_Dto;
+
 public interface ByeongDaon_IService {
 
+	public String idCheckAdmin(String adm_id);
+	public Admin_Dto login_Admin(Map<String, String> map);
+	public Admin_Dto myPageAdmin(String adm_id);
+	public List<Admin_Dto> allAdmin();
 }
