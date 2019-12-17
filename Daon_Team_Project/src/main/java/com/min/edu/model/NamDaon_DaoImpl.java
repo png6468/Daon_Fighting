aa82a597914df405a332e2caa765238ff3159611
png6468 <png6468@gmail.com>
2019-12-17 10:41:57 +0900

    추가 셋팅 - 남규

--- Daon_Team_Project/src/main/java/com/min/edu/model/NamDaon_DaoImpl.java
+++ Daon_Team_Project/src/main/java/com/min/edu/model/NamDaon_DaoImpl.java
@@ -1,5 +1,11 @@
 package com.min.edu.model;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class NamDaon_DaoImpl implements NamDaon_IDao {
 
+	
+	
+	
 }
--- Daon_Team_Project/src/main/java/com/min/edu/model/NamDaon_ServiceImpl.java
+++ Daon_Team_Project/src/main/java/com/min/edu/model/NamDaon_ServiceImpl.java
@@ -1,5 +1,8 @@
 package com.min.edu.model;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NamDaon_ServiceImpl implements NamDaon_IService {
 
 }
