688160d2d91c06ddcfb3f7312aef3cc1f4adefde
greenbean94 <wanjong31@naver.com>
2019-12-18 14:45:29 +0900

    mapper.xml 수정

--- Daon_Team_Project/src/main/java/com/min/edu/model/WanDaon_DaoImpl.java
+++ Daon_Team_Project/src/main/java/com/min/edu/model/WanDaon_DaoImpl.java
@@ -1,5 +1,18 @@
 package com.min.edu.model;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class WanDaon_DaoImpl implements WanDaon_IDao {
 
+	
+	private final Logger logger = LoggerFactory.getLogger(YunDaon_DaoImpl.class);
+	private final String ns = "com.min.edu.model.WanDaon_DaoImpl.wanjong.";
+	
+	@Autowired
+	private SqlSessionTemplate session;
+	
+	
 }
