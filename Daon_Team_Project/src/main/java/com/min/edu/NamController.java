be717a83b44b7051e3e03d680f687f580c96dd3b
png6468 <png6468@gmail.com>
2019-12-18 15:52:00 +0900

    Controller(문제풀기) 작성 시작 - 남규

--- Daon_Team_Project/src/main/java/com/min/edu/NamController.java
+++ Daon_Team_Project/src/main/java/com/min/edu/NamController.java
@@ -2,32 +2,36 @@ package com.min.edu;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.min.edu.dtos.DescPortSel_Dto;
+import com.min.edu.dtos.SelectSel_Dto;
+import com.min.edu.model.NamDaon_IService;
 
 @Controller
 public class NamController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(NamController.class);
 	
+	@Autowired
+	private NamDaon_IService service;
 	
-	@RequestMapping(value = "/init.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@RequestMapping(value = "/poolgi.do", method = RequestMethod.GET)
+	public String moonjepoolgi(String cur_code, Model model) {
+		logger.info("moonjepoolgi {}.");
+		List<SelectSel_Dto> listS = service.gaekExasel(cur_code);
+		List<DescPortSel_Dto> listD = service.sesulExasel(cur_code);
+		model.addAttribute("lists", listS);
+		model.addAttribute("lists", listD);
+		return "Poolgi";
 	}
 }
